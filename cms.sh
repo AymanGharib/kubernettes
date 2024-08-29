3e4312a53bd1adeb07fb9b0caba13ab3cb315bcd
ayman <ayman.gharib@etu.uae.ac.ma>
2024-08-29 04:41:46 +0100

    dd

--- cms.sh
+++ cms.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 
+# Print current context and namespace
+kubectl config current-context
+kubectl config view --minify
+
+# Test access to Kubernetes API
+kubectl cluster-info
+
+# Apply manifests with debugging
+kubectl apply -f deployement.yaml --validate=false
+kubectl apply -f service.yaml --validate=false
 
 
-kubectl apply -f deployement.yaml
 
-kubectl apply -f service.yaml
